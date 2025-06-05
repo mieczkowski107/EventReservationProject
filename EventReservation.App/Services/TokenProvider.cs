e0eb6efe5c3d80168b9891df4ef800096448f346
lmieczkowski <mieczkowski107@gmail.com>
2025-06-05 19:04:59 +0200

    Removed unnecessary using directives. Updated constructor syntax in TokenProvider.cs for clarity.

--- EventReservation.App/Services/TokenProvider.cs
+++ EventReservation.App/Services/TokenProvider.cs
@@ -1,10 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
-using EventReservation.Models;
 using Microsoft.IdentityModel.Tokens;
-using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace EventReservation.App.Services;
 
