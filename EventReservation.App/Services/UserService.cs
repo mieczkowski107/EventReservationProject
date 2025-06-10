7612995a2a7ddeab4460b57fc8ff1307e9269124
lmieczkowski <mieczkowski107@gmail.com>
2025-06-10 16:30:00 +0200

    Add UserService with GetUserId method The class features a static method `GetUserId` that retrieves and parses the user's ID from a `ClaimsPrincipal` object, returning it as a nullable `Guid`. If parsing fails, the method returns `null`.

---
+++ EventReservation.App/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace EventReservation.App.Services;
+
+public class UserService
+{
+    public static Guid? GetUserId(ClaimsPrincipal user)
+    {
+        Guid.TryParse(user.FindFirstValue(ClaimTypes.NameIdentifier), out Guid userId);
+        return userId;
+    }
+}
